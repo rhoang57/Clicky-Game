cb7ce7c876105553c6b95909188d1f1b93c4064e
rhoang57 <rhoang57@gmail.com>
2019-02-13 15:21:38 -0800

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-# Clicky-Game
-A memory game created with React. This app's UI is broken into components, manages component states, and responds to user events.
---
+++ precache-manifest.785ef8039f227432bce5312c2190046f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f9ed21177874c072eef",
+    "url": "/Clicky-Game/static/js/runtime~main.2f9ed211.js"
+  },
+  {
+    "revision": "5de33784d9401b463eb4",
+    "url": "/Clicky-Game/static/js/main.5de33784.chunk.js"
+  },
+  {
+    "revision": "8049a5204cbae57fc29f",
+    "url": "/Clicky-Game/static/js/1.8049a520.chunk.js"
+  },
+  {
+    "revision": "5de33784d9401b463eb4",
+    "url": "/Clicky-Game/static/css/main.e10552d3.chunk.css"
+  },
+  {
+    "revision": "230b7fa590963594ff4d16adb1cf5583",
+    "url": "/Clicky-Game/index.html"
+  }
+];
